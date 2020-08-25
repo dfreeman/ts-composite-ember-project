3cc15f7ce11c06d6c19cbf11cbe48ee74a34e94d
Preston Sego <preston.sego@crowdstrike.com>
2020-08-25 15:23:51 -0400

    fix: addon subpaths were not resolving

--- addon-test-support/index.ts
+++ addon-test-support/index.ts
@@ -1,4 +1,6 @@
 import { value } from 'ts-composite-addon';
+import { foo } from 'ts-composite-addon/foo/folder/foo';
 
 export const addonValue = value;
+export const testFoo = foo;
 export const testValue = 'hi';
