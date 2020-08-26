5c3c81ffd2443eaa569a851c030e844467e63815
Dan Freeman <dfreeman@salsify.com>
2020-08-26 15:20:25 +0200

    Wire composite-app into the build

--- packages/ts-composite-app/app/app.ts
+++ packages/ts-composite-app/app/app.ts
@@ -3,6 +3,16 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ts-composite-app/config/environment';
 
+import { value } from 'ts-composite-addon';
+import { nestedAddonModuleA } from 'ts-composite-addon/dir/nested-module';
+import { testValue } from 'ts-composite-addon/test-support';
+import { nestedTestSupportA } from 'ts-composite-addon/test-support/dir/nested-module';
+
+console.log({ value, testValue, nestedAddonModuleA, nestedTestSupportA });
+
+// @ts-expect-error: the dummy app should only be resolvable from within itself
+import type {} from 'dummy/app';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
